85b44e50325e9803b5f0243a910b1f6b3eaa72f8
Christian Porsch <107266969+chrisPorsch@users.noreply.github.com>
2024-07-28 17:34:38 +0200

    first commit

---
+++ js/summary.js
@@ -0,0 +1,15 @@
+function changeToDoIconOnMouseEnter() {
+    document.getElementById('summary-to-do-icon').src = "img/summary-to-do-hover.svg";
+}
+
+function changeToDoIconOnMouseLeave() {
+    document.getElementById('summary-to-do-icon').src = "img/summary-to-do.svg";
+}
+
+function changeDoneIconOnMouseEnter() {
+    document.getElementById('summary-done-icon').src = "img/summary-done-hover.svg";
+}
+
+function changeDoneIconOnMouseLeave() {
+    document.getElementById('summary-done-icon').src = "img/summary-done.svg";
+}
